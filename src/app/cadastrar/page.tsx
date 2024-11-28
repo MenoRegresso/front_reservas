314ca31b04260630726164bd4fd41527a4558be5
MenoRegresso <pablovinicius4568@gmail.com>
2024-11-28 16:26:31 -0400

    Update page.tsx

--- src/app/cadastrar/page.tsx
+++ src/app/cadastrar/page.tsx
@@ -7,7 +7,8 @@ function Cadastrar() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleSubmit = (e) => {
+ 
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     console.log('Nome:', name);
